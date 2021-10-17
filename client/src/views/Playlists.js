8d62d79c461a32d4c3e591b79c5a1ac1c5d269aa
Anna Erkers <idril.celebrindal@gmail.com>
2021-10-17 12:13:52 +0200

    Add Playlists view

---
+++ client/src/views/Playlists.js
@@ -0,0 +1,36 @@
+import PlaylistCard from "../components/PlaylistCard/PlaylistCard";
+import PlaylistForm from "../components/PlaylistForm/PlaylistForm";
+import { usePlaylists } from "../context/playlistContext";
+import AddSVG from "../components/Icons/AddSVG";
+import styles from "./Playlists.module.scss";
+
+function Playlists() {
+  const {
+    state: { playlists = [] },
+    createPlaylist
+  } = usePlaylists();
+
+  return (
+    <>
+      <div className={styles.header}>
+        <h2>Playlists</h2>
+        <PlaylistForm
+          heading="New playlist"
+          icon={AddSVG}
+          handler={createPlaylist}
+          label="Name"
+        />
+      </div>
+
+      <ul className={styles.playlistWrapper}>
+        {playlists.length
+          ? playlists.map(playlist => (
+              <PlaylistCard playlist={playlist} key={playlist.id} />
+            ))
+          : null}
+      </ul>
+    </>
+  );
+}
+
+export default Playlists;
