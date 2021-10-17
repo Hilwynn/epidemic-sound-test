3a9797767072ff819436102de65f1a2f445093f4
Anna Erkers <idril.celebrindal@gmail.com>
2021-10-17 12:12:12 +0200

    Add custom icons for AudioPlayer

---
+++ client/src/components/AudioPlayer/customIcons.js
@@ -0,0 +1,24 @@
+import ForwardSVG from "../Icons/ForwardSVG";
+import NextSVG from "../Icons/NextSVG";
+import PauseSVG from "../Icons/PauseSVG";
+import PlayCircleSVG from "../Icons/PlayCircleSVG";
+import PreviousSVG from "../Icons/PreviousSVG";
+import RepeatOneSVG from "../Icons/RepeatOneSVG";
+import RewindSVG from "../Icons/RewindSVG";
+import VolumeSVG from "../Icons/VolumeSVG";
+import VolumeMutedSVG from "../Icons/VolumeMutedSVG";
+
+export const customIcons = {
+  play: <PlayCircleSVG />,
+  pause: <PauseSVG />,
+  rewind: <RewindSVG />,
+  forward: <ForwardSVG />,
+  previous: <PreviousSVG />,
+  next: <NextSVG />,
+  loop: <RepeatOneSVG />,
+  loopOff: <RepeatOneSVG />,
+  volume: <VolumeSVG />,
+  volumeMute: <VolumeMutedSVG />
+};
+
+export default customIcons;
